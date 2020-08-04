5aa7fadcacc358d0baf39278c4f2c94385539aa4
zhoumengxiong <zhoumengxiong@outlook.com>
2020-08-04 17:29:45 +0800

    初始化flask-moment扩展。

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+moment = Moment(app)
 
 login.login_view = 'login'
 
