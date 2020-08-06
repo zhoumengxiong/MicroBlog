bc52027ed83cdb71b3294a544b35cc5991180751
zhoumengxiong <zhoumengxiong@outlook.com>
2020-08-06 22:53:06 +0800

    说说表单label改成中文。

--- app/forms.py
+++ app/forms.py
@@ -46,8 +46,8 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
+    post = TextAreaField('发表心情', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('提交')
 
 
 class ResetPasswordRequestForm(FlaskForm):
